e673434be82f093bf8c17966b73fd41372df79ac
Kirill2034 <dum15.00@mail.ru>
2022-06-19 20:14:57 +0400

    Create noteApp

---
+++ src/pages/About.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export const About = () => (
+    <div className='jumbotron'>
+        <div className='container'>
+            <h1 className='display-4'>React test</h1>
+            <p className='lead'>Версия <strong>1.1</strong></p>
+        </div>
+    </div>
+);
